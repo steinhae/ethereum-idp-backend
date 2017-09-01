89d63424fecf2f63f0e48dcfeb61e27093453231
Anton Widera <anton.widera@tum.de>
2017-09-01 17:49:45 +0900

    Added more code comments for inline documentation

--- src/Report.sol
+++ src/Report.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.11;
 
+
+/* This contract class exists, because it should be possible to remove a deployed contract again */
 contract Mortal {
     /* Define variable owner of the type address*/
     address owner;
@@ -18,8 +20,10 @@ contract Mortal {
 }
 
 
+/* Report object is used to store all information about a report*/
 contract Report {
 
+    /*hard coded number of confirmations*/
     uint constant CONFIRMATION_NUMBER = 5;
 
     bytes20 id;
@@ -32,6 +36,8 @@ contract Report {
     bytes32 longitude;
     bytes32 latitude;
 
+    /* description cut into 4 chunks, because it is not possible to
+     * transfer Dynamically-sized byte arrays (strings) between contracts*/
     bytes32 description1;
     bytes32 description2;
     bytes32 description3;
@@ -45,6 +51,7 @@ contract Report {
     address[] fixConfirmations;
     bool enoughFixConfirmations;
 
+    /* initializes a Report with the information it needs*/
     function Report(address sender, bytes32 firstPictureHash, bytes32 secondPictureHash, bytes32 longi, bytes32 lat, bytes32 desc1, bytes32 desc2, bytes32 desc3, bytes32 desc4) {
         id = bytes20(keccak256(longi, lat, firstPictureHash, msg.sig, msg.sender, block.blockhash(block.number - 1)));
         timeStamp = now;
@@ -62,6 +69,11 @@ contract Report {
         description4 = desc4;
     }
 
+    /* adds confirmation to this report object.
+     * checks if sender already confirmed or if sender is the reporter
+     * if report has CONFIRMATION_NUMBER many confirmations after confirming
+     * already confirmed flag will be set*/
+
     function addConfirmation(address sender) public{
         if(sender != reporter && !enoughConfirmations){
             bool alreadyConfirmed = false;
@@ -74,7 +86,6 @@ contract Report {
 
             if (!alreadyConfirmed) {
                 confirmations.push(sender);
-                /* hardcoded limit of confirmations is 5*/
                 if (confirmations.length == CONFIRMATION_NUMBER){
                     enoughConfirmations = true;
                 }
@@ -82,9 +93,43 @@ contract Report {
         }
     }
 
+    /*analogous to addConfirmation but only works if report is fixed*/
+    function addFixConfirmation(address sender) public{
+        if(sender != fixReporter && fixConfirmations.length < CONFIRMATION_NUMBER && fixedReport){
+            bool alreadyConfirmed = false;
+            for (uint256 i; i < fixConfirmations.length; i++) {
+                if (fixConfirmations[i] == sender) {
+                    alreadyConfirmed = true;
+                    break;
+                }
+            }
+            if (!alreadyConfirmed) {
+                fixConfirmations.push(sender);
+                if(fixConfirmations.length == CONFIRMATION_NUMBER){
+                    enoughFixConfirmations = true;
+                }
+            }
+        }
+    }
+
+	
+    /*contractors can set a report to fixed with the picture of the fixed object if it has enough confirmations*/
+    function reportFix(bytes32 hash1, bytes32 hash2) public {
+        if(enoughConfirmations){
+            fixedReport = true;
+            fixHash1 = hash1;
+            fixHash2 = hash2;
+            fixReporter = msg.sender;
+        }
+    }
+
+
+
     function setIncentivePaidToReporter(){
         incentivePaidToReporter = true;
     }
+
+    /*getter for attributes of report object*/
 
     function getIncentivePaidToReporter() public constant returns (bool) {
         return incentivePaidToReporter;
@@ -142,41 +187,12 @@ contract Report {
         return timeStamp;
     }
 
-
-    function reportFix(bytes32 hash1, bytes32 hash2) public {
-        if(enoughConfirmations){
-            fixedReport = true;
-            fixHash1 = hash1;
-            fixHash2 = hash2;
-            fixReporter = msg.sender;
-        }
-    }
-
     function getFixedPictureHash1() public constant returns (bytes32) {
         return fixHash1;
     }
 
     function getFixedPictureHash2() public constant returns (bytes32) {
         return fixHash2;
-    }
-
-    function addFixConfirmation(address sender) public{
-        if(sender != fixReporter && fixConfirmations.length < CONFIRMATION_NUMBER && fixedReport){
-            bool alreadyConfirmed = false;
-            for (uint256 i; i < fixConfirmations.length; i++) {
-                if (fixConfirmations[i] == sender) {
-                    alreadyConfirmed = true;
-                    break;
-                }
-            }
-            if (!alreadyConfirmed) {
-                fixConfirmations.push(sender);
-                /* hardcoded limit of confirmations is 5*/
-                if(fixConfirmations.length == CONFIRMATION_NUMBER){
-                    enoughFixConfirmations = true;
-                }
-            }
-        }
     }
 
     function getEnoughFixConfirmations() public constant returns (bool){
@@ -195,97 +211,22 @@ contract Report {
 
 contract Repairchain is Mortal {
 
+    /* stores all the reports sorted by city */
     mapping (string => Report[]) reports;
+    /* stores all the report IDs sorted by city */
     mapping (string => bytes20[]) ids;
+    /* stores all the cities enabled to store reports */
     string[] cities;
+    /* stores the balance of each city */
     mapping (string => uint) private balances;
-
-    function Repairchain () {
-
-    }
-
-    function addFundsToCity(string city) payable public {
-        if (cityExist(city)) {
-            balances[city] += msg.value;
-        }
-    }
-
-    function addCity (string city) payable public {
-        cities.push(city);
-        balances[city] += msg.value;
-    }
-
-    function getCityBalance(string city) constant returns (uint) {
-        return balances[city];
-    }
-
-    function stringToBytes32(string memory source, uint start) private returns (bytes32 result) {
-        uint startIndex = 32*start;
-        assembly {
-            result := mload(add(source, startIndex))
-        }
-    }
-
-    function secondStringToBytes32(string memory source) private returns (bytes32 result) {
-        assembly {
-            result := mload(add(source, 64))
-        }
-    }
-
-    function bytes32ToString(bytes32 x) private constant returns (string) {
-        bytes memory bytesString = new bytes(32);
-        uint charCount = 0;
-        for (uint j = 0; j < 32; j++) {
-            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-            if (char != 0) {
-                bytesString[charCount] = char;
-                charCount++;
-            }
-        }
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (j = 0; j < charCount; j++) {
-            bytesStringTrimmed[j] = bytesString[j];
-        }
-        return string(bytesStringTrimmed);
-    }
-
-    function compare(string _a, string _b) private returns (int) {
-        bytes memory a = bytes(_a);
-        bytes memory b = bytes(_b);
-        uint minLength = a.length;
-        if (b.length < minLength) minLength = b.length;
-        //@todo unroll the loop into increments of 32 and do full 32 byte comparisons
-        for (uint i = 0; i < minLength; i ++)
-            if (a[i] < b[i])
-                return -1;
-            else if (a[i] > b[i])
-                return 1;
-            if (a.length < b.length)
-                return -1;
-            else if (a.length > b.length)
-                return 1;
-            else
-                return 0;
-    }
-
-    function equal(string _a, string _b) private returns (bool) {
-        return compare(_a, _b) == 0;
-    }
-
-    function cityExist(string city) private returns (bool){
-        bool cityExists = false;
-        for (uint256 i; i < cities.length; i++) {
-            if (equal(cities[i], city)) {
-                cityExists = true;
-                break;
-            }
-        }
-        return cityExists;
-    }
-
+	
+	
+	/*initializes new report object with given parameters and adds it to a city if this city exists*/
     function addReportToCity(string city, string pictureHash, string longitude, string latitude, string description) {
         bool cityExists = cityExist(city);
         if(cityExists){
+			
+			/* multiple variables for same attribute because it is not possible jet to pass dynamic size arrays (strings) betreen contracts*/
             bytes32 pictureHashAsBytes32first = stringToBytes32(pictureHash,1);
             bytes32 pictureHashAsBytes32second = stringToBytes32(pictureHash, 2);
             bytes32 longi = stringToBytes32(longitude, 1);
@@ -299,11 +240,62 @@ contract Repairchain is Mortal {
             reports[city].push(newReport);
         }
     }
-
-    function getReporter(string city, bytes20 id) constant returns (address) {
-        return getReport(city, id).getReporter();
+	
+	/*checks if city exists in the contract*/
+    function cityExist(string city) private returns (bool){
+        bool cityExists = false;
+        for (uint256 i; i < cities.length; i++) {
+            if (equal(cities[i], city)) {
+                cityExists = true;
+                break;
+            }
+        }
+        return cityExists;
+    }
+	
+	function addCity (string city) payable public {
+        cities.push(city);
+        balances[city] += msg.value;
     }
 
+    /* if city exists in the system it can add fonds this way*/
+    function addFundsToCity(string city) payable public {
+        if (cityExist(city)) {
+            balances[city] += msg.value;
+        }
+    }
+
+    function getCityBalance(string city) constant returns (uint) {
+        return balances[city];
+    }
+
+    /*wrapper for addConfirmation function of Report identified by city and id*/
+    function addConfirmationToReport(string city, bytes20 id) public {
+        getReport(city, id).addConfirmation(msg.sender);
+    }
+	
+	/*wrapper for addFixConfirmation function of Report identified by city and id */
+    function addFixConfirmationToReport(string city, bytes20 id) public {
+        Report report = getReport(city, id);
+        report.addFixConfirmation(msg.sender);
+        // pay incentive to reporter
+        if (report.getEnoughFixConfirmations() && !report.getIncentivePaidToReporter() && balances[city] > 5 finney) {
+            report.setIncentivePaidToReporter();
+            balances[city] -= 5 finney;
+            report.getReporter().transfer(5 finney);
+        }
+    }
+
+    /*wrapper for reportFix function of Report Class*/
+    function reportFix (string city, bytes20 id, string pictureHash) public {
+        bytes32 pictureHashAsBytes32first = stringToBytes32(pictureHash, 1);
+        bytes32 pictureHashAsBytes32second = stringToBytes32(pictureHash, 2);
+        getReport(city, id).reportFix(pictureHashAsBytes32first, pictureHashAsBytes32second);
+    }
+
+	
+
+    /*returns report identified with city and id*/
     function getReport(string city, bytes20 id) private constant returns (Report) {
         //iterate over Reports list and return Report with id == id
         Report[] storage cityReports = reports[city];
@@ -314,9 +306,10 @@ contract Repairchain is Mortal {
         }
     }
 
-    //returns true if the confirmation was added and false if already enough confirmations exist
-    function addConfirmationToReport(string city, bytes20 id) public {
-        getReport(city, id).addConfirmation(msg.sender);
+    /*simple getter wrapper functions*/
+
+    function getReporter(string city, bytes20 id) constant returns (address) {
+        return getReport(city, id).getReporter();
     }
 
     function getConfirmationCount (string city, bytes20 id) public constant returns (uint256) {
@@ -371,7 +364,6 @@ contract Repairchain is Mortal {
         return getReport(city, id).getTimestamp();
     }
 
-
     function getFixedPictureHash1 (string city, bytes20 id) public constant returns (string) {
         bytes32 hash = getReport(city, id).getFixedPictureHash1();
         return bytes32ToString(hash);
@@ -386,19 +378,12 @@ contract Repairchain is Mortal {
         return reports[city].length;
     }
 
-
     function getReportIdsFromCity (string city) constant returns (bytes20[]) {
         Report[] storage cityReports = reports[city];
         for (uint i = 0; i < cityReports.length; i++) {
             ids[city].push(cityReports[i].getId());
         }
         return ids[city];
-    }
-
-    function reportFix (string city, bytes20 id, string pictureHash) public {
-        bytes32 pictureHashAsBytes32first = stringToBytes32(pictureHash, 1);
-        bytes32 pictureHashAsBytes32second = stringToBytes32(pictureHash, 2);
-        getReport(city, id).reportFix(pictureHashAsBytes32first, pictureHashAsBytes32second);
     }
 
     function getFixConfirmationCount (string city, bytes20 id) public constant returns (uint256) {
@@ -409,20 +394,63 @@ contract Repairchain is Mortal {
         return getReport(city, id).getEnoughFixConfirmations();
     }
 
-    function addFixConfirmationToReport(string city, bytes20 id) public {
-        Report report = getReport(city, id);
-        report.addFixConfirmation(msg.sender);
-        // pay incentive to reporter
-        if (report.getEnoughFixConfirmations() && !report.getIncentivePaidToReporter() && balances[city] > 5 finney) {
-            report.setIncentivePaidToReporter();
-            balances[city] -= 5 finney;
-            report.getReporter().transfer(5 finney);
+    function getFixedReport(string city, bytes20 id) public constant returns (bool) {
+        return getReport(city, id).getFixedReport();
+    }	
+	
+	
+	/*======Helpers Functons========*/
+
+    function stringToBytes32(string memory source, uint start) private returns (bytes32 result) {
+        uint startIndex = 32*start;
+        assembly {
+            result := mload(add(source, startIndex))
         }
     }
 
-    function getFixedReport(string city, bytes20 id) public constant returns (bool) {
-        return getReport(city, id).getFixedReport();
+
+    function bytes32ToString(bytes32 x) private constant returns (string) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
     }
+
+    
+
+    function compare(string _a, string _b) private returns (int) {
+        bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint minLength = a.length;
+        if (b.length < minLength) minLength = b.length;
+        //@todo unroll the loop into increments of 32 and do full 32 byte comparisons
+        for (uint i = 0; i < minLength; i ++)
+            if (a[i] < b[i])
+                return -1;
+            else if (a[i] > b[i])
+                return 1;
+            if (a.length < b.length)
+                return -1;
+            else if (a.length > b.length)
+                return 1;
+            else
+                return 0;
+    }
+
+    function equal(string _a, string _b) private returns (bool) {
+        return compare(_a, _b) == 0;
+    }
+
 }
 
 
